17e055b7acff852d50ff8286b65ddb059a0ce692
Vu Nguyen <vu0680@gmail.com>
2025-08-10 10:59:32 -0700

    Separate user data to valid and invalid users

--- tests/signin.spec.ts
+++ tests/signin.spec.ts
@@ -3,24 +3,21 @@ import { SignInPage } from '../pages/SignInPage';
 import { getUserFromCSVByIndex } from '../utils/dataProcessing';
 
 const userIndex = process.env.USER_INDEX ? parseInt(process.env.USER_INDEX) : 0;
-const user = getUserFromCSVByIndex('users.csv', userIndex);
+const validUser = getUserFromCSVByIndex('valid_users.csv', userIndex);
+const invalidUser = getUserFromCSVByIndex('invalid_users.csv', userIndex);
 
-test.describe(`Tests for user: ${user.lastName}`, () => {
+test.describe(`Tests for Sign In page`, () => {
   test('Verify user can log in with valid information', async ({ page }) => {
     const signinPage = new SignInPage(page);
     await signinPage.goto();
-    await signinPage.signIn(user);
+    await signinPage.signIn(validUser);
     await signinPage.expectSignOutVisible();
   });
 
   test('Verify user cannot log in with invalid information', async ({ page }) => {
     const signinPage = new SignInPage(page);
     await signinPage.goto();
-    await signinPage.signIn({
-      ...user,
-      licenceNumber: '1234566',
-      keyword: 'wrong'
-    });
+    await signinPage.signIn(invalidUser);
     await signinPage.expectErrorVisible();
   });
 });
--- utils/dataProcessing.ts
+++ utils/dataProcessing.ts
@@ -3,24 +3,7 @@ import path from 'path';
 import { parse } from 'csv-parse/sync';
 import { User } from '../types/user';
 
-// export function readUsersFromCSV(filePath: string): Promise<User[]> {
-//   return new Promise((resolve, reject) => {
-//     const users: User[] = [];
-//     fs.createReadStream(filePath)
-//       .pipe(parse({ columns: true, trim: true }))
-//       .on('data', (row) => {
-//         users.push({
-//           lastName: row.lastName,
-//           licenseNumber: row.licenseNumber,
-//           keyword: row.keyword,
-//         });
-//       })
-//       .on('end', () => resolve(users))
-//       .on('error', (error) => reject(error));
-//   });
-// }
-
-export function readUsersFromCSV(fileName: string): User[] {
+export function getUsersFromCSV(fileName: string): User[] {
   const csvFilePath = path.resolve('data', fileName);
   const csvContent = readFileSync(csvFilePath, 'utf-8');
   return parse(csvContent, {
